e731e446ee70026e066b8f249016f1f796067e89
DESKTOP-LO788SI\Juan Pablo <jupa.1996@hotmail.com>
2018-04-11 12:00:10 -0500

    prueba de onepage con sec_inicio y sec_espacios

--- js/main.js
+++ js/main.js
@@ -1,6 +1,34 @@
 
+window.onload = init;
+
+var sec_inicio;
+var sec_espacios;
+
+var btsec_inicio;
+var btsec_espacios;
+
 function init(){
-    console.log("'Sup! Window has been loaded!");
+   inicializarVariables();
+   inicializarEventos();
 }
 
-window.onload = init;
+function inicializarVariables(){
+    sec_inicio = document.getElementById("inicioCont");
+    sec_espacios = document.getElementById("espaciosCont");
+
+    btsec_inicio = document.getElementById("btsec_inicio");
+    btsec_espacios = document.getElementById("btsec_espacios");
+}
+
+function inicializarEventos(){
+    btsec_inicio.addEventListener("click",procesarClickInicio);
+    btsec_espacios.addEventListener("click",procesarClickEspacios);
+}
+
+function procesarClickInicio(evt){
+    sec_espacios.className = "hidden";
+}
+
+function procesarClickEspacios(evt){
+    sec_inicio.className = "hidden";
+}
